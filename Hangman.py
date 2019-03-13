f4437251b173a3d09c1b56899c361690e89d0e54
Ryan Brasseaux <43624863+BroCode23@users.noreply.github.com>
2019-03-13 13:40:59 -0500

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -1,130 +1,109 @@
 import random
 import sys
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def guess_sort(guessed):
+    """Function to add guesses to the list and then sort the list"""
+    guesses.append(guessed)
+    guesses.sort()
+    return None
+
+def pick_word(file_name):
+    """Opens the file and reads the word list for a word to be picked"""
+    word_list = []
+    with open(file_name, "r") as hgm:
+        for line in hgm:
+            line = line.split()
+            word_list.append(str.strip(line[0])) #takes the words and puts it in a list
+    return word_list[random.randint(0, len(lines))] #picks a random word from the list
+
+def draw_hangman(limbs_lost):
+    """prints out the visual for the hangman"""
+    if limbs_lost < 7:
+        print(" ______    ")
+        print("|      |   ")
+        if limbs_lost == 6:
+            print("|          ")
+        if limbs_lost == 5:
+            print("|      O   ")
+        if limbs_lost == 4:
+            print("|    __O   ")
+        if limbs_lost <= 3:
+            print("|    __O__ ")
+        if limbs_lost > 2:
+            print("|          ")
+            print("|          ")
+            print("|          ")
+        else:
+            print("|      |   ")
+            print("|      ^   ")
+        if limbs_lost == 1:
+            print("|     /    ")
+        if limbs_lost == 0:
+            print("|     / \\ ")
+        print("|__________")
+    return None
+
+def print_display(visual, limbs_lost, list_of_guesses):
+    """prints out the information for the player"""
+    print("\n" + " ".join(visual))
+    print("lives remaining:", limbs_lost)
+    print("you have guessed", ", ".join(list_of_guesses))
+    return None
+
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 lines = []
 guesses = []
 found = []
-space = "_"
-s = " "
-comma = ", "
 lives = 7
-in_file = open("Hgm.txt", "rt")
-with open("Hgm.txt", "rt"):   #opens and reads the word list
-    for line in in_file:
-        line = line.split()
-        lines.append(str.strip(line[0])) #takes the words and puts it in a list
-word = lines[random.randint(0, 1525)] #picks a random word from the list
+
+#Starts game
 print("H A N G M A N")
+word = pick_word("Hgm.txt")
 print("the word has", len(word), "letters.")
-picture = [space] * len(word) #makes the "picture" for the user to see
-print(s.join(picture))
+picture = ["_"] * len(word)
+print(" ".join(picture)) #makes the "picture" for the user to see
+
+#Game loop
 while lives > 0:
-    if picture.count(space) == 0:  #checks if there are any empty spaces left
-        print("congratulations! you have won!")
-        print("   ----      ----  ")
-        print("  (  * )     (  * )")
-        print("                   ")
-        print("          ^        ")
-        print("      \_______/    ")
+    if picture.count("_") == 0:  #checks if there are any empty spaces left
+        print()
+        print("congratulations! you won!")
         sys.exit()
     else:
-        guess = input("what is your guess?\n")
-        guess = guess[:1]
-        fin = word.find(guess)
-        fins = word.find(guess,fin + 1)
-        fint = word.find(guess,fins + 1)
-        if guesses.count(guess) > 0:
+        #Inputs a new guess
+        guess = input("what is your guess?\n")[0]
+        if guess == "":
+            print("Don't just hit enter, guess something!")
+            continue
+        x = 0
+        index = []
+        # finds all occurrences of the guessed letter in the word
+        while word.find(guess, x) != -1:
+            x = word.find(guess,x) + 1
+            if x > 0:
+                index.append(x - 1)
+        #Removes a life if the guess isn't in the word, otherwise adds it to the list of found words
+        if guesses.count(guess):
             print("you already guessed this letter!")
-        elif alpha.count(guess) == 0:
+        #failsafes if the player doesn't type a letter
+        elif letters.count(guess) == 0:
             print("type a letter ya goof")
-        elif fin < 0:
+        elif not len(index): #if the word wasn't found
             lives -= 1
-            guesses.append(guess)
-            guesses.sort()
+            guess_sort(guess)
             print("you guessed incorrectly.")
-        elif fin >= 0 and fins < 0:
-            guesses.append(guess)
-            guesses.sort()
-            found.append(fin)
-            picture[fin] = guess
-        elif fin >= 0 and fins >= 0 and fint < 0:
-            guesses.append(guess)
-            guesses.sort()
-            found.append(fin)
-            found.append(fins)
-            picture[fin] = guess
-            picture[fins] = guess
+        #adds the items to the word
         else:
-            guesses.append(guess)
-            guesses.sort()
-            found.append(fin)
-            found.append(fins)
-            found.append(fint)
-            picture[fin] = guess
-            picture[fins] = guess
-            picture[fint] = guess
-        print()
-        print(s.join(picture))
-        print("lives remaining:", lives)
-        print("you have guessed", comma.join(guesses))
-        if lives == 6:
-            print(" ______    ")
-            print("|      |   ")
-            print("|  ")
-            print("|  ")
-            print("|  ")
-            print("|  ")
-            print("|__________")
-        elif lives == 5:
-            print(" ______    ")
-            print("|      |   ")
-            print("|      O")
-            print("|    ")
-            print("|    ")
-            print("|    ")
-            print("|__________")
-        elif lives == 4:
-            print(" ______    ")
-            print("|      |   ")
-            print("|    __O")
-            print("|        ")
-            print("|        ")
-            print("|   ")
-            print("|__________")
-        elif lives == 3:
-            print(" ______    ")
-            print("|      |   ")
-            print("|    __O__ ")
-            print("|      ")
-            print("|      ")
-            print("|      ")
-            print("|__________")
-        elif lives == 2:
-            print(" ______    ")
-            print("|      |   ")
-            print("|    __O__ ")
-            print("|      |   ")
-            print("|      ^   ")
-            print("|   ")
-            print("|__________")
-        elif lives == 1:
-            print(" ______    ")
-            print("|      |   ")
-            print("|    __O__ ")
-            print("|      |   ")
-            print("|      ^   ")
-            print("|     /  ")
-            print("|__________")
-        elif lives == 0:
-            print(" ______    ")
-            print("|      |   ")
-            print("|    __O__ ")
-            print("|      |   ")
-            print("|      ^   ")
-            print("|     / \\ ")
-            print("|__________")
+            guess_sort(guess)
+            for item in index:
+                found.append(item)
+                picture[item] = guess
+
+        #Prints the display
+        print_display(picture, lives, guesses)
+
+        #draws out the hangman
+        draw_hangman(lives)
+
 print("you lose, better luck next time.")
 print("the word was",word)
-
-
-
