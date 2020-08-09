d48fe35e248654642a567190855c1475d402c2db
Ryan Brasseaux <rbrasseaux1@gmail.com>
2020-08-09 18:01:33 -0500

    restructure code

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,14 @@
-import random
-import sys
+from random import randint
 
-def guess_sort(guessed):
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def add_guess_and_sort(guessed, guesses):
     """Function to add guesses to the list and then sort the list"""
     guesses.append(guessed)
     guesses.sort()
-    return None
+
 
 def pick_word(file_name):
     """Opens the file and reads the word list for a word to be picked"""
@@ -13,8 +16,20 @@ def pick_word(file_name):
     with open(file_name, "r") as hgm:
         for line in hgm:
             line = line.split()
-            word_list.append(str.strip(line[0])) #takes the words and puts it in a list
-    return word_list[random.randint(0, len(word_list))] #picks a random word from the list
+            # takes the words and puts it in a list
+            word_list.append(str.strip(line[0]))
+    # picks a random word from the list
+    return word_list[randint(0, len(word_list) - 1)]
+
+
+def find_letters_in_word(index, guess, word):
+    """finds all occurrences of the guessed letter in the word"""
+    x = word.find(guess, 0)
+    while x >= 0:
+        index.append(x)
+        x += 1
+        x = word.find(guess, x)
+
 
 def draw_hangman(limbs_lost):
     """prints out the visual for the hangman"""
@@ -41,69 +56,66 @@ def draw_hangman(limbs_lost):
         if limbs_lost == 0:
             print("|     / \\ ")
         print("|__________")
-    return None
+
 
 def print_display(visual, limbs_lost, list_of_guesses):
     """prints out the information for the player"""
     print("\n" + " ".join(visual))
     print("lives remaining:", limbs_lost)
     print("you have guessed", ", ".join(list_of_guesses))
-    return None
 
-letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-lines = []
-guesses = []
-found = []
-lives = 7
 
-#Starts game
-print("H A N G M A N")
-word = pick_word("Hgm.txt")
-print("the word has", len(word), "letters.")
-picture = ["_"] * len(word)
-print(" ".join(picture)) #makes the "picture" for the user to see
+def hangman_game(word_list):
+    """game of hangman! try to guess the word without letting your guy die"""
+    guesses = []
+    lives = 7
 
-#Game loop
-while lives > 0:
-    if picture.count("_") == 0:  #checks if there are any empty spaces left
-        print()
-        print("congratulations! you won!")
-        sys.exit()
-    else:
-        #Inputs a new guess
-        guess = input("what is your guess?\n")[0]
-        if guess == "":
+    # Starts game
+    print("H A N G M A N")
+    word = pick_word(word_list)
+    print("the word has", len(word), "letters.")
+    picture = ["_"] * len(word)
+    print(" ".join(picture))  # makes the "picture" for the user to see
+
+    # Game loop
+    while lives > 0 and picture.count("_") != 0:
+        # Inputs a new guess
+        guess = input("what is your guess?\n")
+        if not guess:
             print("Don't just hit enter, guess something!")
             continue
-        x = 0
-        index = []
-        # finds all occurrences of the guessed letter in the word
-        while word.find(guess, x) != -1:
-            x = word.find(guess,x) + 1
-            if x > 0:
-                index.append(x - 1)
-        #Removes a life if the guess isn't in the word, otherwise adds it to the list of found words
-        if guesses.count(guess):
-            print("you already guessed this letter!")
-        #failsafes if the player doesn't type a letter
-        elif letters.count(guess) == 0:
+        elif guess not in LETTERS:
             print("type a letter ya goof")
-        elif not len(index): #if the word wasn't found
-            lives -= 1
-            guess_sort(guess)
-            print("you guessed incorrectly.")
-        #adds the items to the word
+            continue
+        elif guess in guesses:
+            print("you already guessed this letter!")
+            continue
         else:
-            guess_sort(guess)
-            for item in index:
-                found.append(item)
-                picture[item] = guess
+            guess = guess[0].lower()
 
-        #Prints the display
+        index = []
+        find_letters_in_word(index, guess, word)
+
+        if len(index) == 0:  # if the letter wasn't found in word
+            lives -= 1
+            print("you guessed incorrectly.")
+        else:  # adds the items to the word
+            for item in index:
+                picture[item] = guess
+        add_guess_and_sort(guess, guesses)
+
+        # Prints the display
         print_display(picture, lives, guesses)
 
-        #draws out the hangman
+        # draws out the hangman
         draw_hangman(lives)
 
-print("you lose, better luck next time.")
-print("the word was",word)
+    if not picture.count("_"):
+        print()
+        print("congratulations! you won!")
+    else:
+        print("you lose, better luck next time.")
+        print("the word was", word)
+
+
+hangman_game('Hgm.txt')
